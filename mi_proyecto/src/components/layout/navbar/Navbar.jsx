94eb89d02d72984f84e6cb980f7e57debe29d864
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2024-03-11 23:11:27 -0300

    Avances 001 - Proyecto Integrador parte 2

--- mi_proyecto/src/components/layout/navbar/Navbar.jsx
+++ mi_proyecto/src/components/layout/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Drawer, IconButton } from "@mui/material";
 import Badge from "@mui/material/Badge";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
@@ -7,17 +8,27 @@ import "./navbar.scss";
 
 const Navbar = () => {
 
+    const [ openDrawer, setOpenDrawer ] = useState(false);
+
     const links = [
         { id: 1, text: "Inicio", url: "/", icon:"" },
         { id: 2, text: "Nosotros", url: "/about", icon:"" },
         { id: 3, text: "Contacto", url: "/contact", icon:"" },
     ];
 
+    const abrir = () => {
+        setOpenDrawer(true);
+    };
+
+    const cerrar = () => {
+        setOpenDrawer(false);
+    };
+
     return (
         <Box className="navbar">
 
             <Box className="navbar__drawer-icon">
-                <MenuOutlinedIcon/>
+                <MenuOutlinedIcon onClick={abrir}/>
             </Box>
             <Box className="navbar__items">
                 {links.map( (link) => {
@@ -31,13 +42,20 @@ const Navbar = () => {
 
             </Box>
             <Box className="navbar__shopping-cart">
-                <Badge
-                    className="navbar__shopping-cart__icon-badge"
-                    badgeContent={6}
-                    color="primary">
-                    <ShoppingCartOutlinedIcon/>
-                </Badge>
+                <IconButton>
+                    <Badge
+                        className="navbar__shopping-cart__icon-badge"
+                        badgeContent={6}
+                        color="primary">
+                        <ShoppingCartOutlinedIcon/>
+                    </Badge>
+                </IconButton>
             </Box>
+            <Drawer
+                open={openDrawer}
+                anchor="left"
+                onClose={cerrar}>
+            </Drawer>
         </Box>
     );
 };
