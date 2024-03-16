e59f9cc597aca89283ead2ad1469cab7bc3abf42
Fernando Aguilar <fernandoaguilar@192.168.1.6>
2024-03-16 20:49:05 -0300

    Avances 005 - Proyecto Integrador 5

--- mi_proyecto/src/components/layout/footer/Footer.jsx
+++ mi_proyecto/src/components/layout/footer/Footer.jsx
@@ -1,10 +1,51 @@
+import { Box, Link } from "@mui/material";
+import { NavLink } from "react-router-dom";
 import "./footer.scss";
 
 const Footer = () => {
+
+    const links = [
+        { id: 1, text: "Inicio", url: "/"},
+        { id: 2, text: "Nosotros", url: "/about"},
+        { id: 3, text: "Contacto", url: "/contact" },
+    ];
+
     return (
-        <footer className="footer">
-            <p>&copy;2023 Todos los derechos reservados</p>
-        </footer>
+        <Box
+            component="footer"
+            className="footer">
+
+            <Box className="footer__groups">
+                <Box>
+                    <h4>Explorar</h4>
+                    <Box className="footer__groups__list">
+                        {links.map( (link) => (
+                            <NavLink
+                                key={link.id}
+                                to={link.url}>
+                                    {link.text}
+                                </NavLink>
+                        ))}
+                    </Box>
+                </Box>
+                <Box>
+                    <h4>Legales</h4>
+                    <Box className="footer__groups__list">
+                        <Link href="https://www.google.com/" > Términos y condiciones </Link>
+                    </Box>
+                </Box>
+                <Box>
+                    <h4>RRSS</h4>
+                    <Box className="footer__groups__list">
+                        <h4>x</h4>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box className="footer__copyrigth">
+                <span>&copy;2023 Todos los derechos reservados</span>
+            </Box>
+        </Box>
     );
 };
 
